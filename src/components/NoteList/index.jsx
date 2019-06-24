8a60669ed4dd055b53ae12869741ca67b8600ad0
Alexander Alarcon <walarcon14@gmail.com>
2019-06-23 20:54:38 -0500

    Create NoteList Component

---
+++ src/components/NoteList/index.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { PropTypes } from 'prop-types';
+
+import Note from '../Note';
+
+function NoteList({ notes }) {
+  const notesList = notes.length ? (
+    notes.map(note => <Note note={note} />)
+  ) : (
+    <p>There are not notes </p>
+  );
+  return <div className="NoteList">{notesList}</div>;
+}
+
+NoteList.propTypes = {
+  notes: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.number,
+      body: PropTypes.string
+    })
+  ).isRequired
+};
+export default NoteList;
--- src/containers/App/index.jsx
+++ src/containers/App/index.jsx
@@ -1,16 +1,26 @@
 import React from 'react';
 
 import './index.css';
-import Note from '../../components/Note';
+import NoteList from '../../components/NoteList';
 
 function App() {
-  const note = {
-    id: 1,
-    body: 'Lorem Ipsum'
-  };
+  const notes = [
+    {
+      id: 1,
+      body: 'Lorem Ipsum'
+    },
+    {
+      id: 2,
+      body: 'Dolor Sit'
+    },
+    {
+      id: 3,
+      body: 'Amet Lorem'
+    }
+  ];
   return (
     <div className="App">
-      <Note note={note} />
+      <NoteList notes={notes} />
     </div>
   );
 }
