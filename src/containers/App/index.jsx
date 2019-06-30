2c9f3ac46c7918a4c6e13377ca98e84193e469ba
Alexander Alarcon <walarcon14@gmail.com>
2019-06-30 07:09:06 -0500

    Create ModalNoteEditor Component Fix Note and NoteEditor Styles

--- src/containers/App/index.jsx
+++ src/containers/App/index.jsx
@@ -1,44 +1,23 @@
 import React, { useContext } from 'react';
+import Modal from 'react-modal';
 
 import './index.css';
 
-import { NoteContext } from '../../store/noteContext';
 import { NotesContext } from '../../store/notesContext';
 
+import ModalNoteEditor from '../ModalNoteEditor';
 import NoteList from '../../components/NoteList';
-import NoteEditor from '../../components/NoteEditor';
+
+Modal.setAppElement('#root');
 
 function App() {
   const {
-    state: { notes },
-    addNote,
-    editNote
+    state: { notes }
   } = useContext(NotesContext);
-  const { note, updateNote, resetNote } = useContext(NoteContext);
 
   return (
     <div className="App">
-      <NoteEditor
-        note={note}
-        handleTextChange={event =>
-          updateNote({ ...note, body: event.target.value })
-        }
-        handleRadioChange={event =>
-          updateNote({
-            ...note,
-            color: event.target.value
-          })
-        }
-        handleSave={() => {
-          if (note.id) {
-            editNote(note);
-          } else {
-            const newNote = { ...note, id: +new Date() };
-            addNote(newNote);
-          }
-          resetNote();
-        }}
-      />
+      <ModalNoteEditor />
       <NoteList notes={notes} />
     </div>
   );
---
+++ src/containers/ModalNoteEditor/index.jsx
@@ -0,0 +1,88 @@
+import React, { useCallback, useContext, useState } from 'react';
+import Modal from 'react-modal';
+
+import './index.css';
+
+import { NoteContext } from '../../store/noteContext';
+import { NotesContext } from '../../store/notesContext';
+
+import NoteEditor from '../../components/NoteEditor';
+import IconButton from '../../components/IconButton';
+
+function ModalNoteEditor() {
+  const { addNote, editNote } = useContext(NotesContext);
+  const { note, updateNote, resetNote } = useContext(NoteContext);
+  const [showModal, setShowModal] = useState(false);
+  const handleClose = useCallback(() => setShowModal(false), []);
+  const handleOpen = useCallback(() => setShowModal(true), []);
+  const handleTextChange = useCallback(
+    event => updateNote({ ...note, body: event.target.value }),
+    [note, updateNote]
+  );
+  const handleRadioChange = useCallback(
+    event =>
+      updateNote({
+        ...note,
+        color: event.target.value
+      }),
+    [note, updateNote]
+  );
+  const handleSave = useCallback(() => {
+    if (note.id) {
+      editNote(note);
+    } else {
+      const newNote = { ...note, id: +new Date() };
+      addNote(newNote);
+    }
+    resetNote();
+    setShowModal(false);
+  }, [addNote, editNote, note, resetNote]);
+
+  return (
+    <React.Fragment>
+      <Modal
+        isOpen={showModal}
+        onRequestClose={handleClose}
+        shouldCloseOnOverlayClick
+        closeTimeoutMS={500}
+        style={{
+          content: {
+            backgroundColor: 'transparent',
+            border: 'none',
+            top: 0,
+            right: 0,
+            bottom: 0,
+            left: 0,
+            padding: 0,
+            width: '100vw',
+            height: '100vh'
+          }
+        }}
+      >
+        <div className="d-table">
+          <div className="d-table-cell">
+            <IconButton
+              iconName="fa-times-circle"
+              disabled={false}
+              onClick={handleClose}
+            />
+            <NoteEditor
+              note={note}
+              handleTextChange={handleTextChange}
+              handleRadioChange={handleRadioChange}
+              handleSave={handleSave}
+            />
+          </div>
+        </div>
+      </Modal>
+
+      <IconButton
+        iconName="fa-plus-circle"
+        disabled={false}
+        onClick={handleOpen}
+      />
+    </React.Fragment>
+  );
+}
+
+export default ModalNoteEditor;
